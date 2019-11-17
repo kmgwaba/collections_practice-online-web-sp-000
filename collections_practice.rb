29913fa13af8f314e525a9c50992712726b03b11
Kathia Mgwaba <mgwaba68@gmail.com>
2019-11-17 20:24:22 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -39,5 +39,11 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect{|element, index|}
+  array.each_with_index.collect do |string, index|
+    if index == 1
+      string
+    else
+      string << "s"
+    end
+  end 
 end
